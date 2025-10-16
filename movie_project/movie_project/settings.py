bec62d72d69aa22a5539439e2228544cbba485a5
Tensura Haritonov <jeskozee@gmail.com>
2025-10-17 00:28:04 +0500

    Initial commit - Django backend

--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +91,7 @@ WSGI_APPLICATION = 'movie_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 AUTH_USER_MODEL = 'users.CustomUser'
