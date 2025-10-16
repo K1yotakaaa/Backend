9190134743539cd978b21cff6730ded71cbdc4bd
Tensura Haritonov <jeskozee@gmail.com>
2025-10-17 00:36:55 +0500

    Initial commit - Django backend

--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -33,6 +33,10 @@ ALLOWED_HOSTS = [
   'https://angular-homework-backend.up.railway.app'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://angular-homework-backend.up.railway.app',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
